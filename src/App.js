28deff9d009a7eb7744c65c8b375941abe9a04c7
jose-benedito <benethowen@yahoo.com.br>
2023-07-03 21:38:40 -0300

    Adicionado navbar e footer na página home e aplicado css.

--- src/App.js
+++ src/App.js
@@ -2,29 +2,28 @@ import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
+import Projects from './components/pages/Projects'
 import NewProject from './components/pages/NewProject'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/Contact">Contato</Link>
-        <Link to="newproject">Novo Projeto</Link>
-      </div>
+      <Navbar />
         <Container customClass="min_height">
       <Routes>
 
           <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />}/>
           <Route path="/newProject" element={<NewProject />}/>
       </Routes>
         </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,11 @@
+function Projects(){
+    return (
+        <>
+        
+        <h1>Projects</h1>
+        <p>Página da Projects</p>
+        
+        </>
+    )
+}
+export default Projects
