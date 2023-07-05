122ae70e2ad868c69e1d6935936b0c9e1b542fdb
jose-benedito <benethowen@yahoo.com.br>
2023-07-05 16:51:30 -0300

    Criado formulário em New Project.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,16 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
 function Home(){
     return (
-        <>
+        <section className={styles.home_container}>
+            <h1>Bem-Vindo ao <span>Costs</span></h1>
+            <p>Comece a gernciar os seus projetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Criar Projeto"/>
+            <img src={savings} alt="Costs" />
+        </section>
         
-        <h1>Home</h1>
-        <p>Página da home</p>
-        
-        </>
     )
 }
 export default Home
