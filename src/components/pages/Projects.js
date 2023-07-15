0e3f50afedfa614bec136c1a53cead4e4074e4a8
jose-benedito <benethowen@yahoo.com.br>
2023-07-14 21:14:11 -0300

    Criado a função de excluir projeto.

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -11,6 +11,7 @@ function Projects(){
 
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message=''
@@ -39,7 +40,21 @@ function Projects(){
         }, 300)
     }, [])
 
+    function removeProject(id){
+        fetch(`http://localhost:5000/projects/${id}`,{
+            
+            method:"DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            },
 
+        }).then(resp => resp.json())
+            .then(() => {
+               setProjects(projects.filter((project)=> project.id !== id)) 
+               setProjectMessage('Projeto removido com sucesso!')
+            })
+        .catch(err => console.log(err))
+    }
 
     return (
         <div className={styles.project_container}>
@@ -50,6 +65,7 @@ function Projects(){
 
         </div>
         {message && <Message type="success" msg={message} />}
+        {projectMessage && <Message type="success" msg={projectMessage} />}
 
         <Container customClass="start">
             {projects.length > 0 &&
@@ -60,6 +76,7 @@ function Projects(){
                 budget={project.budget}
                category={project.category.name}
                 key={project.id}
+                handleRemove={removeProject}
                 />
             ))}
 
